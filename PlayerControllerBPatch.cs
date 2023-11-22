f9cac559c64689428725ae9163bfc5e8cecf3ef0
FFGFlash <26831072+FFGFlash@users.noreply.github.com>
2023-11-22 03:12:13 -0600

    Infinite sprint tutorial

---
+++ PlayerControllerBPatch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using GameNetcodeStuff;
+
+namespace FlashUtils.Patches;
+
+[HarmonyPatch(typeof(PlayerControllerB))]
+internal class PlayerControllerBPatch
+{
+  [HarmonyPatch("Update")]
+  [HarmonyPostfix]
+  private static void patchUpdate(ref float ___sprintMeter) {
+    ___sprintMeter = 1f;
+  }
+}
---
+++ Plugin.cs
@@ -0,0 +1,20 @@
+﻿using BepInEx;
+using FlashUtils.Patches;
+using HarmonyLib;
+
+namespace FlashUtils
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new(PluginInfo.PLUGIN_GUID);
+        private static Plugin Instance;
+
+        private void Awake()
+        {
+            if (Instance == null) Instance = this;
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
