8ebb64a606420c96d78ffd7232f7ecedce1fc341
FFGFlash <26831072+FFGFlash@users.noreply.github.com>
2023-11-22 03:50:57 -0600

    fixed naming scheme

--- PlayerControllerBPatch.cs
+++ PlayerControllerBPatch.cs
@@ -8,7 +8,7 @@ internal class PlayerControllerBPatch
 {
   [HarmonyPatch("Update")]
   [HarmonyPostfix]
-  private static void patchUpdate(ref float ___sprintMeter)
+  private static void PatchUpdate(ref float ___sprintMeter)
   {
     if (Plugin.Instance.configInfiniteSprint.Value) ___sprintMeter = 1f;
   }
